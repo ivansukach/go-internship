e12381e201683baa378f0440f0ccca3d05308754
Ivan Sukach <vsukach1999@gmail.com>
2021-05-07 12:12:41 +0300

    change project structure

--- reverse-strings/go.mod
+++ reverse-strings/go.mod
@@ -1,4 +1,4 @@
-module github.com/ivansukach/reverse-strings
+module	github.com/ivansukach/go-internship/reverse-strings
 
 go 1.16
 
---
+++ reverse-strings/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/ivansukach/go-internship/reverse-strings/tools"
+	"golang.org/x/exp/errors/fmt"
+)
+
+func main() {
+	fmt.Println(tools.Reverse("Hello, world!"))
+}
