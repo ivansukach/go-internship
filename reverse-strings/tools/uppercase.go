cf3b357028a382266172ae6c2f344f5b188b3aad
Ivan Sukach <vsukach1999@gmail.com>
2021-05-12 13:17:48 +0300

    optimize UpperCase

--- reverse-strings/tools/uppercase.go
+++ reverse-strings/tools/uppercase.go
@@ -1,8 +1,20 @@
 package tools
 
-import "strings"
+import (
+	"unicode"
+)
 
 // UpperCase converts string to upper case.
 func UpperCase(in string) string {
-	return strings.ToUpper(in)
+	tmp := make([]rune, len(in))
+	amt := 0
+	for _, v := range in {
+		tmp[amt] = v
+		amt++
+	}
+	tmp = tmp[:amt]
+	for i, v := range tmp {
+		tmp[i] = unicode.ToUpper(v)
+	}
+	return string(tmp)
 }
