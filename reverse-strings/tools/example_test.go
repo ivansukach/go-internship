a3a459967cc0ca94054b1b4bf81810827f933faf
Ivan Sukach <vsukach1999@gmail.com>
2021-05-12 13:11:20 +0300

    add UpperCase func and example tests

---
+++ reverse-strings/tools/example_test.go
@@ -0,0 +1,15 @@
+package tools
+
+import "fmt"
+
+// ExampleReverse demonstrates the work of Reverse func.
+func ExampleReverse() {
+	fmt.Println(Reverse("hello"))
+	// Output: olleh
+}
+
+// ExampleUpperCase demonstrates the work of UpperCase func.
+func ExampleUpperCase() {
+	fmt.Println(UpperCase("hello"))
+	// Output: HELLO
+}
