c0f8c00ac61e2884cd657a88227ab2292417dc85
Ivan Sukach <vsukach1999@gmail.com>
2021-05-12 13:21:42 +0300

    remove unnecessary interations

--- reverse-strings/tools/uppercase.go
+++ reverse-strings/tools/uppercase.go
@@ -9,12 +9,8 @@ func UpperCase(in string) string {
 	tmp := make([]rune, len(in))
 	amt := 0
 	for _, v := range in {
-		tmp[amt] = v
+		tmp[amt] = unicode.ToUpper(v)
 		amt++
 	}
-	tmp = tmp[:amt]
-	for i, v := range tmp {
-		tmp[i] = unicode.ToUpper(v)
-	}
-	return string(tmp)
+	return string(tmp[:amt])
 }
