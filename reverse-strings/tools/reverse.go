a2a04e2e8c875737e5813d292ceaede5ada0327f
Ivan Sukach <vsukach1999@gmail.com>
2021-05-12 11:32:52 +0300

    change comments format

--- reverse-strings/tools/reverse.go
+++ reverse-strings/tools/reverse.go
@@ -1,9 +1,7 @@
-/*
-Package tools implements a simple library, that contains tools for work with strings.
-*/
+// Package tools implements a simple library, that contains tools for work with strings.
 package tools
 
-//Reverse is a function that reverses a string
+// Reverse is a function that reverses a string.
 func Reverse(in string) (result string) {
 	for _, v := range in {
 		result = string(v) + result
